3613e09fb57c07db6dad4206e408ad63d8ddd6fe
CebotariValerii <valera20022002@gmail.com>
2023-11-13 21:04:40 +0200

    my first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// "use sctrict"
+// const question = confirm('Are you here?');
+
+// console.log(question)
+
+// const answer = prompt('do you have 18 years old?', 'yes')
+// console.log(typeof(answer))
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', "")
+answers[1] = prompt('Как ваша фамилия?', "")
+answers[2] = prompt('Сколько вам лет?', "")
+
+document.write(answers);
